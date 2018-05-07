4085043bdca03e596061a1baf862c66d4877fe28
cperchik <32876209+cperchik@users.noreply.github.com>
2018-05-07 10:27:08 -0400

    finished

--- sum35While.js
+++ sum35While.js
@@ -3,8 +3,46 @@
 
 function sum35While(N) {
     let counter = 1;
+    let multiples = 0;
+    let sum = 0;
 
-    while(counter < N = 1)
+    while (counter < N) {
+        let div3
+        let div5
+
+        if (counter % 3 === 0) {
+            div3 = true;
+        } else {
+            div3 = false;
+        }
+
+
+        if (counter % 5 === 0) {
+            div5 = true;
+        } else {
+            div5 = false;
+        }
+
+
+        if(div5 == true){
+            multiples += 1;
+            sum += counter;
+        }
+        if(div3 == true) {
+            multiples +=1;
+            sum += counter;
+        }
+        if(div3 && div5 == true) {
+            multiples -= 1
+            sum -= counter
+        }
+        counter += 1;
+    }
+
+    console.log("There are " + multiples + " multiples of 3 and 5 that are less than " + N + ".");
+    console.log("Their sum is " +  sum +".");
+}
+
 
 
 //Try out your function here.
